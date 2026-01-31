8913438e8b896401fff8532df170259a8147aa02
Ndukuba Nneoma Chidinma <59185484+Thatchidinma@users.noreply.github.com>
2026-01-31 13:13:29 +0100

    feat: add live chat popup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import localFont from 'next/font/local'
+import { Messages3 } from "iconsax-reactjs";
 
 export const metadata: Metadata = {
   title: "Protech Dashboard",
@@ -25,7 +26,11 @@ export default function RootLayout({
       >
         <NavBar/>
         {children}
+              <div className="fixed bottom-20 right-20 p-[16.6px] rounded-full bg-[#242526]">
+        <Messages3 variant="Bold" color="#ffffff"/>
+      </div>
       </body>
+
     </html>
   );
 }
